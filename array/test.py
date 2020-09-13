50c71cce390fb570bbd60993067c8126aa4cdfc5
ds-praveenkumar <praveenkr2208@gmail.com>
2020-09-13 18:46:12 +0530

    added problems

--- array/test.py
+++ array/test.py
@@ -2,6 +2,8 @@ import unittest
 
 from p1_lcs import LCS
 from p2 import SubArraySum
+from p3 import MaximumSubSum
+from p4 import MissingNumber
 
 
 class TestLCS(unittest.TestCase):
@@ -56,6 +58,43 @@ class TestLCS(unittest.TestCase):
         p2 = SubArraySum(arr=seq, sum=sum)
         res = p2.sub_array_sum()
         self.assertEqual(res, -1)
+    
+    def test_p3_case1(self):
+        seq = [1,2,4,4]
+        p3 = MaximumSubSum(arr=seq)
+        res = p3.max_sub_sum()
+        self.assertEqual(res, 11)
+
+    def test_p3_case2(self):
+            seq = [-1,2,4,4]
+            p3 = MaximumSubSum(arr=seq)
+            res = p3.max_sub_sum()
+            self.assertEqual(res, 9)
+
+    def test_p3_case3(self):
+            seq = [-1,-2,-4,-4]  
+            p3 = MaximumSubSum(arr=seq)
+            res = p3.max_sub_sum()
+            self.assertEqual(res, -1)
+
+    def test_p4_case1(self):
+            seq = [1,2,4,5]  
+            p3 = MissingNumber(arr=seq)
+            res = p3.one_missing_number()
+            self.assertEqual(res, 3)
+
+    def test_p4_case2(self):
+            seq = [1,2,6,5,4]  
+            p3 = MissingNumber(arr=seq)
+            res = p3.one_missing_number()
+            self.assertEqual(res, 3)
+    
+    def test_p4_case3(self):
+            seq = [1,2,6,5]  
+            p3 = MissingNumber(arr=seq)
+            res = p3.multiple_missing_number(6)
+            self.assertEqual(res, {3,4})
+
 
 if __name__ == '__main__':
     unittest.main()
